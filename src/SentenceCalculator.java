7877294b122e621cc328cd1edeac3e2a088643bd
tbaharvand <tarabaharvand97@gmail.com>
2025-06-26 17:20:39 +0330

    optimize map

--- src/SentenceCalculator.java
+++ src/SentenceCalculator.java
@@ -10,35 +10,14 @@ public class SentenceCalculator {
             System.out.println(c + "="+ ascii);
             if (ascii>=65 && ascii <= 90){
                     Map<Integer , Integer > letterMap = new HashMap<>();
-                    letterMap.put(65,2);
-                    letterMap.put(66,4);
-                    letterMap.put(67,6);
-                    letterMap.put(68,8);
-                    letterMap.put(69,10);
-                    letterMap.put(70,12);
-                    letterMap.put(71,14);
-                    letterMap.put(72,16);
-                    letterMap.put(73,18);
-                    letterMap.put(74,20);
-                    letterMap.put(75,22);
-                    letterMap.put(76,24);
-                    letterMap.put(77,26);
-                    letterMap.put(78,28);
-                    letterMap.put(79,30);
-                    letterMap.put(80,32);
-                    letterMap.put(81,34);
-                    letterMap.put(82,36);
-                    letterMap.put(83,38);
-                    letterMap.put(84,40);
-                    letterMap.put(85,42);
-                    letterMap.put(86,44);
-                    letterMap.put(87,46);
-                    letterMap.put(88,48);
-                    letterMap.put(89,50);
-                    letterMap.put(90,52);
+                    int score = 2;
+                    for (int asciiForCapitalAtoZ = 65 ; asciiForCapitalAtoZ<= 90 ; asciiForCapitalAtoZ++){
+                        letterMap.put(asciiForCapitalAtoZ,score);
+                        score = score + 2;
+                    }
 
                 int total = letterMap.get(ascii);
-                System.out.println("letterMap is "+ total );
+                System.out.println("score is "+ total );
                 sum += total;
                 System.out.println("sum is " + sum);
 
@@ -47,36 +26,14 @@ public class SentenceCalculator {
             else if (ascii>=97 && ascii<=122){
 
                 Map<Integer , Integer > letterMap = new HashMap<>();
-
-              letterMap.put(97,1);
-              letterMap.put(98,2);
-              letterMap.put(99,3);
-              letterMap.put(100,4);
-              letterMap.put(101,5);
-              letterMap.put(102,6);
-              letterMap.put(103,7);
-              letterMap.put(104,8);
-              letterMap.put(105,9);
-              letterMap.put(106,10);
-              letterMap.put(107,11);
-              letterMap.put(108,12);
-              letterMap.put(109,13);
-              letterMap.put(110,14);
-              letterMap.put(111,15);
-              letterMap.put(112,16);
-              letterMap.put(113,17);
-              letterMap.put(114,18);
-              letterMap.put(115,19);
-              letterMap.put(116,20);
-              letterMap.put(117,21);
-              letterMap.put(118,22);
-              letterMap.put(119,23);
-              letterMap.put(120,24);
-              letterMap.put(121,25);
-              letterMap.put(122,26);
+                int score = 1;
+                for (int asciiForSmallAtoZ = 97; asciiForSmallAtoZ <= 122 ; asciiForSmallAtoZ++){
+                    letterMap.put(asciiForSmallAtoZ,score);
+                    score = score + 1 ;
+                }
 
                 int total = letterMap.get(ascii);
-                System.out.println("letterMap is "+ total );
+                System.out.println("score is "+ total );
                 sum += total;
                 System.out.println("sum is " + sum);
 
@@ -84,21 +41,14 @@ public class SentenceCalculator {
             else if (ascii>=48 && ascii<=57){
 
                 Map<Integer , Integer > letterMap = new HashMap<>();
+                int score = 0;
+                for (int asciiForNumbers0to9 = 48 ; asciiForNumbers0to9 <= 57 ; asciiForNumbers0to9++){
+                    letterMap.put(asciiForNumbers0to9,score);
+                    score +=1;
 
-                letterMap.put(48,0);
-                letterMap.put(49,1);
-                letterMap.put(50,2);
-                letterMap.put(51,3);
-                letterMap.put(52,4);
-                letterMap.put(53,5);
-                letterMap.put(54,6);
-                letterMap.put(55,7);
-                letterMap.put(56,8);
-                letterMap.put(57,9);
-
-
+                }
                 int total = letterMap.get(ascii);
-                System.out.println("letterMap is "+ total );
+                System.out.println("score is "+ total );
                 sum += total;
                 System.out.println("sum is " + sum);
 
@@ -113,6 +63,6 @@ public class SentenceCalculator {
     }
 
     public static void main(String[] args) {
-            lettersToNumbers("Baharvand");
+            lettersToNumbers("A7");
     }
 }
